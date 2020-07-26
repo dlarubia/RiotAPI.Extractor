df1433a8f61d11018ae313f2e89db2fa07fd428e
Daniel La Rubia <daniel-la-rubia@hotmail.com>
2020-07-26 19:27:22 -0300

    MATCH-V4

--- RiotAPI.Learning/Models/MatchFrame.cs
+++ RiotAPI.Learning/Models/MatchFrame.cs
@@ -1,4 +1,9 @@
-﻿namespace RiotAPI.Learning.Models {
+﻿using System.Collections.Generic;
+
+namespace RiotAPI.Learning.Models {
     public class MatchFrame {
+        public Dictionary<string, MatchParticipantFrame> participantFrames { get; set; }
+        public List<MatchEvent> events { get; set; }
+        public long timestamp { get; set; }
     }
 }
